355aa79aafeef75e0ba6926889c6affe4274d62b
mahadi215 <mahadihasanbd215@gmail.com>
2022-10-12 14:35:52 +0300

    quiz d

--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -4,9 +4,12 @@ const SingleQuiz = ({q}) => {
     const {question,id,correctAnswer,options} = q;
     return (
         <div className='quiz-box'>
+            <div>
+                <button className='c-ans'><i class="fa-solid fa-eye"></i></button>
+            </div>
             <p className='question'>{question}</p>
             {
-                options.map(option => <div>
+                options.map(option => <div className='radio-box'>
                     <input type="radio" id={id} name="1" value={option}/>
                     <label for="1">{option}</label>
                   </div>)
