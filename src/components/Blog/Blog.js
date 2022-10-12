2b10b9231589e63bbddd5935db12d22586238d90
mahadi215 <mahadihasanbd215@gmail.com>
2022-10-12 17:36:06 +0300

    final

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,8 +3,14 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h2>blog</h2>
+            <h2>what is the perpouse of react router</h2>
+            <p>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+Let us create a simple application to React to understand how the React Router works. The application will contain three components: home component, about a component, and contact component. We will use React Router to navigate between these components.</p>
+        <h2>how doer context api works</h2>
+        <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
         </div>
+
+
     );
 };
 
